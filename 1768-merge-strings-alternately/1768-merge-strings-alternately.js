44a40dfb59218359aac4873fa2b1e742c949001d
Athena <85322728+WillSaveTime@users.noreply.github.com>
2023-04-24 13:20:02 -0700

    Time: 54 ms (81.99%), Space: 41.8 MB (85.83%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -6,18 +6,11 @@
 var mergeAlternately = function(word1, word2) {
     let result = "";
     
-    if (word1.length <= word2.length) {
-        for (let i = 0; i < word1.length; i++) {
-            result += word1[i].concat(word2[i]);
-        }
-        result = result.concat(word2.slice(word1.length, word2.length))
-    } 
-    if (word1.length > word2.length) {
-        for (let i = 0; i < word2.length; i++) {
-            result += word1[i].concat(word2[i]);
-        }
-        console.log(result, word1.slice(word2.length, word1.length))
-        result = result.concat(word1.slice(word2.length, word1.length))
+    const maxSize = Math.max(word1.length, word2.length);
+    
+    for (let i = 0; i < maxSize; i++) {
+        if (i < word1.length) result += word1[i];
+        if (i < word2.length) result += word2[i];
     }
     return result;
 };
